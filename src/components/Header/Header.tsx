46a87986f44b8545afe6ab4bcfb585c446b63054
toMilanesi <tomi.milanesi@gmail.com>
2022-12-01 14:00:35 -0300

    Add headeer nav:

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-
-type Props = {}
-
-const Header = (props: Props) => {
+const Header = () => {
   return (
-    <header className='flex justify-around items-center bg-slate-600 text-white h-[50px]'>
+    <header className='flex justify-around items-center bg-[#FFF059] text-gray-500 h-[50px]'>
       <div>
         <h1>Logo</h1>
       </div>
+      <div>
+        <input type="text" name="search" />
+      </div>
       <ul className='flex gap-5'>
-        <li>Home</li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#products">Products</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
     </header>
   )
